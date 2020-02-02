d927500bb8258855f833c654e16d6b022dd6d154
Andrew <522988922@qq.com>
2020-02-02 23:03:15 +0800

    swagger2

--- src/main/java/com/example/springboot/demo/Ctl.java
+++ src/main/java/com/example/springboot/demo/Ctl.java
@@ -1,5 +1,6 @@
 package com.example.springboot.demo;
 
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.java.Log;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,15 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 @Log
 public class Ctl {
 
-    @RequestMapping("/hello")
+    @ApiOperation("删除用户1")
+    @RequestMapping("/hello1")
     public String sayHello(@Validated User user) {
-        log.info("hello");
+        log.info("hello1");
         return "谢谢观看" + user.getName();
     }
 
+    @ApiOperation("删除用户2")
     @RequestMapping("/hello2")
     public String sayHello2(User user) {
-        log.info("hello");
+        log.info("hello2");
         return "谢谢观看" + user.getName();
     }
 }
