8a019d58ebb282eb9956b11b4065506d094d664d
Andrew <522988922@qq.com>
2020-02-21 01:07:50 +0800

    test

--- src/main/java/com/example/springboot/demo/DemoApplication.java
+++ src/main/java/com/example/springboot/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 public class DemoApplication {
 
-    // mvn clean package docker:build
+    // mvn clean package
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
