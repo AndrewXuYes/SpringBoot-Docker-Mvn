43cde35b13aab4a9f51cf5d92a181dc0e1ce4484
Andrew <522988922@qq.com>
2020-02-20 13:00:32 +0800

    精简代码

--- src/main/java/com/example/springboot/demo/DemoApplication.java
+++ src/main/java/com/example/springboot/demo/DemoApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class DemoApplication {
 
     // mvn clean package docker:build
