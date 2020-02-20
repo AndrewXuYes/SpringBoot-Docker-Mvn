03bb351c64fff9cad0db332bd8bfd71f88afad6e
Andrew <522988922@qq.com>
2020-02-20 12:46:12 +0800

    精简代码

--- src/main/java/com/example/springboot/demo/DemoApplication.java
+++ src/main/java/com/example/springboot/demo/DemoApplication.java
@@ -1,17 +1,14 @@
 package com.example.springboot.demo;
 
-import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@EnableAdminServer
 public class DemoApplication {
 
     // mvn clean package docker:build
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
-
 
 }
