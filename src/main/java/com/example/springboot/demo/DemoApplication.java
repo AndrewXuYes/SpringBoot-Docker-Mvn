449522ddd417c1b9e233c59fd4f29404c8ed2265
Andrew <522988922@qq.com>
2020-02-21 17:26:28 +0800

    fail-end

--- src/main/java/com/example/springboot/demo/DemoApplication.java
+++ src/main/java/com/example/springboot/demo/DemoApplication.java
@@ -1,10 +1,11 @@
 package com.example.springboot.demo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-
+@MapperScan("com.example.springboot.demo.mapper")
 public class DemoApplication {
 
     // mvn clean package
