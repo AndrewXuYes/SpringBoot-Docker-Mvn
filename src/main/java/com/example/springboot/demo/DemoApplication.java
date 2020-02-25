ecf69188285d4e07c5703c3e9883be9ae30518c9
Andrew <522988922@qq.com>
2020-02-25 22:37:19 +0800

    项目结束

--- src/main/java/com/example/springboot/demo/DemoApplication.java
+++ src/main/java/com/example/springboot/demo/DemoApplication.java
@@ -5,12 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-    /**
-     * TODO
-     * 微服务监控
-     *
-     */
-
 
     // mvn clean package
     public static void main(String[] args) {
